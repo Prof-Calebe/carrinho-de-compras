fd3ffa79bbc35c93bbc3bdc57fee41436e8dd8d7
1130664 <1130664@090094-P33S14.macklabs.br>
2013-03-26 20:41:54 -0300

    Corrige o uso do objetos mocks com PowerMock e EasyMock.

--- test/br/calebe/exemplos/ex02/ClasseExemploTest.java
+++ test/br/calebe/exemplos/ex02/ClasseExemploTest.java
@@ -15,21 +15,27 @@ import org.powermock.modules.junit4.PowerMockRunner;
  * @author Calebe de Paula Bianchini
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ClasseExemploController.class})
+@PrepareForTest({ClasseExemplo.class})
 public class ClasseExemploTest {
-
-    @Before
-    public void before() throws Exception {
-        ClasseExemploController controller = PowerMock.createMock(ClasseExemploController.class);
-        PowerMock.expectNew(ClasseExemploController.class);
-        EasyMock.expect(controller.metodo(10)).andReturn("Resposta: 10");
-        PowerMock.replayAll();
-    }
 
     @Test
     public void executandoClasseExemplo() throws Exception {
-        ClasseExemplo e = new ClasseExemplo();
-        e.run(10);
-        Assert.assertEquals("Resposta: 10", e.getAnswer());
+        // Cria o objeto Mock da classe ClasseExemploController
+        ClasseExemploController controllerMock = PowerMock.createMock(ClasseExemploController.class);
+        // Espera que toda instanciação dessa classe seja substituída pelo objeto mockado
+        PowerMock.expectNew(ClasseExemploController.class).andReturn(controllerMock);
+        // E espera que a resposta pela chamada do método seja determinado
+        EasyMock.expect(controllerMock.metodo(10)).andReturn("Resposta: 10");
+        // "Executa" a configuração programada
+        PowerMock.replay(controllerMock, ClasseExemploController.class);
+        
+        // Chama a classe - internamente, a classe mockada será utilizada
+        ClasseExemplo tested = new ClasseExemplo();
+        tested.run(10);
+        
+        // Faz a verificaçao agendada
+        Assert.assertEquals("Resposta: 10", tested.getAnswer());
+        // Executa todas as verificação
+        PowerMock.verifyAll();
     }
 }
